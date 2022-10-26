e2873b02a639f3f59e669ef56b58372200da6638
codershrutik <72141758+codershrutik@users.noreply.github.com>
2022-10-26 22:32:09 +0530

    Create basiccalc.py

---
+++ basiccalc.py
@@ -0,0 +1,14 @@
+a = float(input("enter a: "))
+b = float(input("enter b: "))
+
+sum = a+b
+dif = a-b
+mult = a*b
+div = a/b
+mod = a%b
+
+print("the sum is ",sum)
+print("the difference is ",dif)
+print("the multiplication is",mult)
+print("the division is ",div)
+print("the remainder is ",mod)
